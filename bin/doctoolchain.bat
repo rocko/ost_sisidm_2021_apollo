1f1f08c373a4b1a873bd855d748bf693f6d9e928
rocko <c_k_a@t-online.de>
2021-05-31 13:25:35 +0200

    b

---
+++ bin/doctoolchain.bat
@@ -0,0 +1,69 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  docToolchain script for Windows
+@rem
+@rem ##########################################################################
+
+set CMD_LINE_ARGS=%*
+
+IF "%1"=="" GOTO :help
+IF [%1]==[/?] GOTO :help
+
+set SCRIPTDIR=%~dp0
+set BASEDIR=%SCRIPTDIR%..\
+set GRADLECMD=%BASEDIR%gradlew.bat
+set WORKINGDIR=%cd%\
+set PATHTODOCS=%1
+
+@REM throw the first parameter away
+set params=%*
+set params=%params:* =%
+
+@REM Execute docToolchain
+
+@REM %GRADLECMD% --project-cache-dir %BASEDIR%.gradle -p %BASEDIR% -PdocDir=%WORKINGDIR%%1 %params%
+
+cd /d %BASEDIR%
+
+IF "%PATHTODOCS:~0,1%"=="." goto :relativePath
+
+call %GRADLECMD% --project-cache-dir %BASEDIR%/.gradle "-PdocDir=%PATHTODOCS%" %params%
+
+goto :end
+
+:relativePath
+
+call %GRADLECMD% --project-cache-dir %BASEDIR%/.gradle "-PdocDir=%WORKINGDIR%%PATHTODOCS%" %params%
+
+goto :end
+
+:help
+
+
+echo Usage: doctoolchain docDir [option...] [task...]
+echo.
+echo docDir - Absolute directory with the documentation.
+echo.
+echo You can use the same options and tasks as in underlying gradle.
+echo Use "doctoolchain . --help" to see available options.
+echo Use "doctoolchain . tasks" to see available tasks.
+echo.
+echo Examples:
+echo.
+echo   Init new project with arc42 tempalte
+echo     doctoolchain . -b init.gradle initArc42EN
+echo.
+echo   Generate PDF:
+echo     doctoolchain . generatePDF
+echo.
+echo   Generate HTML:
+echo     doctoolchain . generateHTML
+echo.
+echo   Publish HTML to Confluence:
+echo     doctoolchain . publishToConfluence
+goto :end
+
+:end
+rem back to WORKINGDIR
+cd /d %WORKINGDIR%
