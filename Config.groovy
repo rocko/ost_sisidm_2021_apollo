e000b9c0f5e3d16a04abaa0ce656159952a080d8
rocko <c_k_a@t-online.de>
2021-06-01 07:28:06 +0200

    [no ci] groovy

--- Config.groovy
+++ Config.groovy
@@ -8,7 +8,6 @@ inputPath = '.';
 
 
 inputFiles = [
-        [file: 'doctoolchain_demo.adoc',       formats: ['html','pdf']],
         [file: 'arc42-template.adoc',    formats: ['html','pdf']],
 ]
 
